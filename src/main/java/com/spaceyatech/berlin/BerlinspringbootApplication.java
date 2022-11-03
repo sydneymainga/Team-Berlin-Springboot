24efc981c1dfdda6a9f93fb6fa6d4cb90ba394bc
Sydney Mainga <user@users-MacBook-Pro.local>
2022-11-03 18:05:36 +0300

    springboot project skeleton

---
+++ src/main/java/com/spaceyatech/berlin/BerlinspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.spaceyatech.berlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerlinspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerlinspringbootApplication.class, args);
+	}
+
+}
