da2886d5a617a594e751add8b7e8df60a095af1a
Sydney Mainga <user@users-MacBook-Pro.local>
2022-11-06 18:22:02 +0300

    slf4j

--- src/main/java/com/spaceyatech/berlin/BerlinspringbootApplication.java
+++ src/main/java/com/spaceyatech/berlin/BerlinspringbootApplication.java
@@ -1,13 +1,16 @@
 package com.spaceyatech.berlin;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class BerlinspringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BerlinspringbootApplication.class, args);
+		log.info("hello");
 	}
 
 }
