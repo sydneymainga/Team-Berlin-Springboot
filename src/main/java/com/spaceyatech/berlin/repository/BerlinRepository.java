eac19d215f5b9fdbdaf2881876f19c2d7827bde9
Sydney Mainga <user@users-MacBook-Pro.local>
2022-11-06 19:17:33 +0300

    repository refinement

--- src/main/java/com/spaceyatech/berlin/repository/BerlinRepository.java
+++ src/main/java/com/spaceyatech/berlin/repository/BerlinRepository.java
@@ -1,7 +1,12 @@
 package com.spaceyatech.berlin.repository;
 
+import com.spaceyatech.berlin.models.Berlin;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
-public class BerlinRepository {
+public interface BerlinRepository extends JpaRepository<Berlin, Integer> {
+// TODO: 06/11/2022 interacts with db queries
+
 }
