ef3367c94ceb4f84ce6f42c9139f9c9edb1157dc
Sydney Mainga <user@users-MacBook-Pro.local>
2022-11-08 17:34:43 +0300

    UUID id generator and id moved to base entity to be used by all models

--- src/main/java/com/spaceyatech/berlin/models/BaseEntity.java
+++ src/main/java/com/spaceyatech/berlin/models/BaseEntity.java
@@ -1,32 +1,26 @@
 package com.spaceyatech.berlin.models;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.*;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.util.UUID;
 
-@Builder
+@MappedSuperclass
 @Data
-@Entity
 @AllArgsConstructor
-public class Users extends BaseEntity{
+@NoArgsConstructor
+public class BaseEntity {
 
-    /*@Id
+    @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(
             name = "UUID",
             strategy = "org.hibernate.id.UUIDGenerator"
     )
-    private UUID userId;*/
-    private String firstname;
-    private String lastname;
-    private String username;
-    private String user_email;
-    private String password;
-
-
+    private UUID Id;
 }
