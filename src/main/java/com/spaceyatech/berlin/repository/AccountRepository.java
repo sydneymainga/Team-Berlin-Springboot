841cab0b73a7fe4f3d3514b9397bbc2c13232c1f
Sydney Mainga <user@users-MacBook-Pro.local>
2022-12-06 23:00:49 +0300

    clean up

--- src/main/java/com/spaceyatech/berlin/repository/AccountRepository.java
+++ src/main/java/com/spaceyatech/berlin/repository/AccountRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -16,5 +15,4 @@ public interface AccountRepository extends JpaRepository<Account, UUID> {
     @Query(value = "SELECT * FROM account where users_id = :userId",nativeQuery = true)
     public List<Account> findByUserId(UUID userId);
 
-    //public Optional<Account> findByUserId(UUID userId);
 }
